72c2bb2eb14bebc76e587ea7842f9a05d9aceed7
JoshuaNascimento <joshua.l.nascimento@gmail.com>
2020-05-17 02:43:06 -0400

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/pages/loginpage/loginpage.component.jsx
+++ src/pages/loginpage/loginpage.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import Login from '../../components/login/login.component';
+import SignUp from '../../components/sign-up/sign-up.component'
 
 import './loginpage.styles.scss';
 
 const LoginPage = () => (
-    <div className='loginpage'><Login/></div>
+    <div className='loginpage'>
+        <Login/>
+        <SignUp/>
+    </div>
 );
 
 export default LoginPage;
